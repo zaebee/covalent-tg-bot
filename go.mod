5f721016ce91a9b07573fdc622be3e7472b7b538
zaebee <zaebuntu@gmail.com>
2021-05-07 14:31:18 +0200

    Added simple bot handler to show balance.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/zaebee/covalent-tg-bot
 
 go 1.16
+
+require (
+	github.com/zaebee/govalent v0.0.0-20210505231852-ee424e57ad61
+	gopkg.in/tucnak/telebot.v2 v2.3.5
+)
--- main.go
+++ main.go
@@ -1,5 +1,58 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/zaebee/govalent"
+	"github.com/zaebee/govalent/class_a"
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
 func main() {
-	return
+	token := os.Getenv("TOKEN")
+	b, err := tb.NewBot(tb.Settings{
+		URL:    "https://api.telegram.org",
+		Token:  token,
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/help", func(m *tb.Message) {
+		msg := `Available commands:
+		/help - show this message
+		/balance <chain_id> <address> - show wallet balances`
+		b.Send(m.Sender, msg)
+	})
+
+	b.Handle("/balance", func(m *tb.Message) {
+		var message string
+		params := strings.Split(m.Payload, " ")
+		log.Printf("got params: %v", params)
+		if len(params) < 2 {
+			message = "Not enough params. Please, set chain_id and address."
+		}
+		if len(params) == 2 {
+			var balances []string
+			info, err := govalent.ClassA().GetTokenBalances(params[0], params[1], class_a.BalanceParams{Nft: false})
+			if err != nil {
+				message = fmt.Sprintf("Sorry, I can't get balance for given address: %v", err)
+			}
+			for _, i := range info.Items {
+				balance := fmt.Sprintf("%v - %v %v", i.ContractName, i.Balance, i.ContractTickerSymbol)
+				balances = append(balances, balance)
+			}
+			message = strings.Join(balances, "\n")
+			message = fmt.Sprintf("Balance: %+v\n%s", info.Address, message)
+		}
+		b.Send(m.Sender, message)
+	})
+
+	b.Start()
 }
